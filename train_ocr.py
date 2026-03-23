18eb7b8b1d7a97c6fefaea6e98c817ac32f144ff
hl9082 <hl9082@g.rit.edu>
2026-03-23 11:57:57 -0400

    Increase Epoch number to 10 for better learning

--- train_ocr.py
+++ train_ocr.py
@@ -37,7 +37,7 @@ MODEL_PATH = os.path.join(MODEL_DIR, "greek_ocr_lenet_fast.pth")
 HF_REPO_ID = "huyisme-005/ancient-greek-ocr_2" # <--- UPDATE THIS!
 
 BATCH_SIZE = 512
-EPOCHS = 5
+EPOCHS = 10
 LEARNING_RATE = 0.002
 IMAGE_SIZE = 32
 
